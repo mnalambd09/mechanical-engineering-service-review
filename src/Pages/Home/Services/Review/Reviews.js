c239d712a85fc3cbc91a0361750f7039dcab6b0b
mnalambd09 <mnalambd09@gmail.com>
2022-11-10 02:45:21 +0600

    toast has been added

--- src/Pages/Home/Services/Review/Reviews.js
+++ src/Pages/Home/Services/Review/Reviews.js
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../../../Context/AuthProvider/AuthProvider';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Reviews = ({ params }) => {
-    const {user} = useContext(AuthContext);
+    const notify = () => toast("Your Review has been placed Successfully");
+    const { user } = useContext(AuthContext);
     const { _id, title, price, picture, rating, description } = useLoaderData();
 
     const handleReview = event => {
@@ -29,22 +32,23 @@ const Reviews = ({ params }) => {
         fetch('http://localhost:5000/review', {
             method: 'POST',
             headers: {
-                'content-type' : 'application/json'
+                'content-type': 'application/json'
             },
             body: JSON.stringify(review)
         })
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                if(data.acknowledged){
-                    alert('Your Review has been placed Successfully')
+                if (data.acknowledged) {
+                    // alert('Your Review has been placed Successfully')
+                    notify()
                     form.reset();
                 }
             })
             .catch(error => console.error(error));
     }
-    
-    
+
+
     return (
         <div className='text-center'>
             <h1 className='text-3xl font-bold py-5 text-orange-600'>Your are about to <span className='text-green-600 font-extrabold'>{title}</span></h1>
@@ -54,15 +58,16 @@ const Reviews = ({ params }) => {
                     <h2 className="card-title">{title}</h2>
                     <p className='text-left mb-5'>{description}</p>
                     <div className='flex flex-auto bg-black py-12 font-bold text-3xl text-orange-600'>
-                    <p>Price : ${price}</p>
-                    <p>Rating : {rating} </p>
+                        <p>Price : ${price}</p>
+                        <p>Rating : {rating} </p>
                     </div>
                     <div className="card-actions justify-end">
-                        
+
                     </div>
                 </div>
             </div>
             <form onSubmit={handleReview} className='rounded-lg shadow-lg shadow-black shadow-inner bg-green-200 mb-5 px-5'>
+            <ToastContainer/>
                 <div className='grid  grid-cols-1 lg:grid-cols-2 gap-4  pt-10 '>
                     <input required name='firstName' type="text" placeholder='First Name' className='input input-ghost w-full border-spacing-2 bg-orange-600 text-white font-extrabold' />
                     <input name='lastName' type="text" placeholder='Last Name' className='input input-ghost w-full border-spacing-2 bg-orange-600 text-white font-extrabold ' />
@@ -70,7 +75,7 @@ const Reviews = ({ params }) => {
                     <input name='url' type="url" placeholder='Image url' className='input input-ghost w-full border-spacing-2 bg-orange-600 text-white font-extrabold ' />
                 </div>
                 <textarea name='message' className="textarea textarea-accent w-full mt-5 border-spacing-2 bg-orange-600 text-white font-extrabold" placeholder="Wright Your Review"></textarea>
-                    <input type="submit" className="btn btn-primary mb-5" value="Submit Your Review" />
+                <input type="submit" className="btn btn-primary mb-5" value="Submit Your Review" />
             </form>
         </div>
     );
